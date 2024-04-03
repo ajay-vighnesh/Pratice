f15663a8f64e9ace49eac0887fd66b595e1ceb28
Ajayvighnesh V <ajay3546@global.chainsys.com>
2024-04-03 17:52:54 +0530

    latest-code

---
+++ Coding challenge/Condition-Operator/app.js
@@ -0,0 +1,5 @@
+var mark = 20;
+var mark1 = 10;
+var a;
+a = mark < mark1 ? "good" : "bad";
+console.log(a);
---
+++ Coding challenge/Condition-Operator/app.ts
@@ -0,0 +1,7 @@
+let mark : number = 20;
+let mark1 : number = 10;
+let a : string;
+
+a = mark<mark1 ? "good" : "bad";
+
+console.log(a)
